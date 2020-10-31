d8d61d01f1bf375e44068f52e1b91f958f49be96
frankvega2010 <franco_vega@msn.com>
2020-10-31 14:11:08 -0300

    Added the ability to spawn villagers from the HQ (50 gold each)

---
+++ RTS-Project/Assets/Scripts/Gameplay/HQ.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HQ : MonoBehaviour
+{
+    public delegate void OnGoldAction();
+    public static OnGoldAction OnGoldDeposit;
+    public static OnGoldAction OnGoldUsed;
+
+    public GameObject villagerPrefab;
+    public float villagerCost;
+    public Transform villagerSpawnPoint;
+    private GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.Get();
+    }
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        
+    }*/
+
+    public void DepositGold(float amount)
+    {
+        gm.gold += amount;
+        if(OnGoldDeposit != null)
+        {
+            OnGoldDeposit();
+        }
+    }
+
+    public void SpawnVillager()
+    {
+        if(gm.gold >= villagerCost)
+        {
+            gm.gold -= villagerCost;
+
+            //Spawn Villager on spawn point
+            GameObject newVillager = Instantiate(villagerPrefab, villagerSpawnPoint.transform.position, villagerSpawnPoint.transform.rotation);
+
+            if (OnGoldUsed != null)
+            {
+                OnGoldUsed();
+            }
+        }
+    }
+}
