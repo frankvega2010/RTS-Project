21ecf0b0e724d914c789d348340dee9c0d2841fe
frankvega2010 <franco_vega@msn.com>
2020-10-31 15:31:53 -0300

    Added basic mine spawner

---
+++ RTS-Project/Assets/Scripts/Gameplay/MineSpawner.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineSpawner : MonoBehaviour
+{
+    public int currentMines;
+    public int maxMines;
+    public float minTime;
+    public float maxTime;
+
+    private Grid grid;
+    private float finalTime;
+    private float spawnTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = Grid.Get();
+        SetNewTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentMines < maxMines)
+        {
+            spawnTimer += Time.deltaTime;
+
+            if(spawnTimer >= finalTime)
+            {
+                SpawnMine();
+                SetNewTimer();
+            }
+        }
+    }
+
+    private void SetNewTimer()
+    {
+        float randomTime = Random.Range(minTime, maxTime);
+        finalTime = randomTime;
+        spawnTimer = 0;
+    }
+
+    private void SpawnMine()
+    {
+        if(grid)
+        {
+            List<Node> allNormalNodes = new List<Node>();
+
+            foreach (Node n in grid.nodes)
+            {
+                OreMine o = n.GetComponent<OreMine>();
+
+                if (!o.enabled)
+                    allNormalNodes.Add(n);
+            }
+
+            int randomIndex = Random.Range(0, allNormalNodes.Count);
+            allNormalNodes[randomIndex].GetComponent<OreMine>().enabled = true;
+            currentMines++;
+
+        }
+    }
+}
---
+++ RTS-Project/Assets/Scripts/Gameplay/OreMine.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OreMine : MonoBehaviour
+{
+    public bool isExplored;
+    public MeshRenderer mesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        // Mine Activated
+        Debug.Log("Mine Activated");
+        mesh.material.color = Color.red;
+        isExplored = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
